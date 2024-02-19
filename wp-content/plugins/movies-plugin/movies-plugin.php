47b15ad185861c852886f47ffbb10424a6f7100d
Michaelaronja <michaela.bang@oakwood.se>
2024-02-19 10:25:26 +0100

    Show CTP in rest args, WIP meta fields

--- wp-content/plugins/movies-plugin/movies-plugin.php
+++ wp-content/plugins/movies-plugin/movies-plugin.php
@@ -19,12 +19,116 @@ class Movie_Fetcher {
         register_deactivation_hook(__FILE__, array($this, 'deactivate'));
 
         add_action('init', array($this, 'register_movie_post_type')); // Registrera posttypen
-        // add_action('init', array($this, 'init')); // Registrera init-metoden som hook för init-evenemanget
+        // add_action('init', array($this, 'register_movie_meta_fields')); // Registrera metafält
     }
 
     public function activate() {
-        // Aktiveringsåtgärder, om några
-        this->init();
+        // Aktiveringsåtgärder, inklusive att skapa posterna
+        $this->create_movies_posts();
+    }
+
+    // private function register_movie_meta_fields() {
+    //     register_post_meta('movie', 'director', array(
+    //         'show_in_rest' => true,
+    //         'single' => true,
+    //         'type' => 'string',
+    //         'description' => 'Director of the movie',
+    //         'auth_callback' => function() {
+    //             return current_user_can('edit_posts');
+    //         }
+    //     ));
+    //     // Här kan du lägga till fler metafält för din posttyp "movie"
+    // }
+
+    private function create_movies_posts() {
+        //Config API Client
+        $this->api_client = new API_Client('https://www2.stockholmfilmfestival.se/graphql');
+
+        //Make GraphQL request and fetch data 
+        $graphql_query = <<<GRAPHQL
+        {
+            products(
+                filter: {
+                    category_id: { eq: "26" }
+                }
+            ) {
+                items {
+                    name
+                    sku
+                    url_key
+                    image {
+                        url
+                    }
+                }
+            }
+        }
+        GRAPHQL;
+        $response = $this->api_client->make_graphql_request($graphql_query);
+        // Hantera API-svar och lagra data i WordPress-databasen
+        if ($response) {
+            $movies_data = json_decode($response, true);
+
+            $movies = $movies_data['data']['products']['items']; 
+
+            if (isset($movies)) {
+                foreach ($movies as $movie) {
+                    // Lagra varje film i WordPress-databasen med hjälp av Data_Handler
+                    $title = $movie['name'];
+                    $sku = $movie['sku'];
+                    $path = $movie['url_key'];
+                    $image_url = $movie['image']['url'];
+
+                    $new_post_id = wp_insert_post(array(
+                        'post_title'   => $title,
+                        'post_type'    => 'movie',
+                        'post_status'  => 'publish',
+                    ));
+
+                    if (!is_wp_error($new_post_id)) {
+                        // Lagra ytterligare information som metadata, t.ex. SKU och bild-URL
+                        add_post_meta($new_post_id, 'sku', $sku);
+                        add_post_meta($new_post_id, 'image_url', $image_url);
+                    
+
+                        // Lägg till en bild som 'featured image' om det finns en URL för det
+                        if (!empty($image_url)) {
+                            $image_data = file_get_contents($image_url);
+
+                            // Skapa en unik filnamn för att undvika kollisioner
+                            $unique_image_url = md5($image_url . time()) . '.jpg';
+
+                            // Spara bilden i uploads-mappen
+                            $upload_dir = wp_upload_dir();
+                            $upload_path = $upload_dir['path'] . '/' . $unique_image_url;
+                            file_put_contents($upload_path, $image_data);
+
+                            // Förbered bilagan för att läggas till i WordPress-media
+                            $attachment = array(
+                                'guid'           => $upload_dir['url'] . '/' . $unique_image_url,
+                                'post_mime_type' => 'image/jpeg',
+                                'post_title'     => $title,
+                                'post_content'   => '',
+                                'post_status'    => 'inherit'
+                            );
+
+                            // Lägg till bilagan i WordPress-media
+                            $attachment_id = wp_insert_attachment($attachment, $upload_path, $new_post_id);
+
+                            if (!is_wp_error($attachment_id)) {
+                                // Sätt bilagan som 'featured image' för posten
+                                set_post_thumbnail($new_post_id, $attachment_id);
+                            } else {
+                                // Hantera fel om bilagan inte kunde läggas till
+                                error_log('Error inserting attachment: ' . $attachment_id->get_error_message());
+                            }
+                        }
+                    } else {
+                        // Hantera fel om posten inte kunde skapas
+                        error_log('Error creating post: ' . $new_post_id->get_error_message());
+                    }
+                }
+            }
+        }
     }
 
     public function deactivate() {
@@ -61,86 +165,25 @@ class Movie_Fetcher {
             'has_archive'        => true,
             'hierarchical'       => false,
             'menu_position'      => null,
-            'supports'           => array('title', 'editor', 'thumbnail', 'excerpt')
+            'supports'           => array('title', 'editor', 'thumbnail', 'excerpt', 'custom-fields'),
+            'show_in_rest'          => true, 
+            'rest_controller_class' => 'WP_REST_Posts_Controller'
         );
 
         register_post_type('movie', $args);
-    }
 
-    public function init() {
-        error_log('Init function is running.', 0, ABSPATH . 'wp-content/debug.log');
+        // Register post meta for movie post type
+        register_post_meta('movie', 'director', array(
+            'show_in_rest' => true,
+            'single' => true,
+            'type' => 'string',
+            'description' => 'Director of the movie',
+            'auth_callback' => function() {
+                return current_user_can('edit_posts');
+            }
+        ));
+    }
     
-
-        //Config API Client
-        $this->api_client = new API_Client('https://www2.stockholmfilmfestival.se/graphql');
-
-        //Make GraphQL request and fetch data 
-        $graphql_query = <<<GRAPHQL
-        {
-            products(
-                filter: {
-                    category_id: { eq: "26" }
-                }
-            ) {
-                items {
-                    name
-                    sku
-                    url_key
-                    image {
-                        url
-                    }
-                }
-            }
-        }
-        GRAPHQL;
-        $response = $this->api_client->make_graphql_request($graphql_query);
-        // Hantera API-svar och lagra data i WordPress-databasen
-        if ($response) {
-            $movies_data = json_decode($response, true);
-
-            $movies = $movies_data['data']['products']['items']; 
-            
-
-            if (isset($movies)) {
-                
-                foreach ($movies as $movie) {
-                    // Lagra varje film i WordPress-databasen med hjälp av Data_Handler
-                    $title = $movie['name'];
-                    $sku = $movie['sku'];
-                    $path = $movie['url_key'];
-                    $image_url = $movie['image']['url'];
-
-                    $new_post_id = wp_insert_post(array(
-                        'post_title'   => $title,
-                        // 'post_content' => $post_content,
-                        'post_type'    => 'movie',
-                        'post_status'  => 'publish',
-                    ));
-
-
-                    if (!is_wp_error($new_post_id)) {
-                        // Lagra ytterligare information som metadata, t.ex. SKU och bild-URL
-                        add_post_meta($new_post_id, 'sku', $movie['sku']);
-                        add_post_meta($new_post_id, 'image_url', $movie['image']['url']);
-
-                        // Lägg till en bild som 'featured image' om det finns en URL för det
-                        if (!empty($image_url)) {
-                            // var_dump($image_url);
-
-                            $image_id = media_sideload_image($image_url, $new_post_id);
-                
-                            if (!is_wp_error($imageUrl)) {
-                                set_post_thumbnail($new_post_id, $image_id);
-                            }
-                        }
-                    } else {
-                        // Hantera fel om posten inte kunde skapas
-                        error_log('Error creating post: ' . $new_post_id->get_error_message());
-                    }
-                }
-            }
-        }
-    }
 }
 
 $movie_fetcher = new Movie_Fetcher();
